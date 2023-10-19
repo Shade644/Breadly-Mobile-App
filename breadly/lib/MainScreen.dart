c4f958cedb4cb9b8789dee09027b06d98195d908
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2023-10-19 19:24:22 +0200

    .

--- breadly/lib/MainScreen.dart
+++ breadly/lib/MainScreen.dart
@@ -6,6 +6,7 @@ import 'package:breadly/ui/searchpage.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:provider/provider.dart';
+import 'BottomNav.dart';
 import 'bottom_nav.dart';
 
 class MainScreen extends StatefulWidget {
@@ -35,50 +36,10 @@ class _MainScreenState extends State<MainScreen> {
     body: pageList[mainScreenNotifier.pageIndex],
 
     
-      bottomNavigationBar: SafeArea(
-        child:  Padding(padding: const EdgeInsets.all(8),
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        margin: const EdgeInsets.symmetric(horizontal: 10),
-        decoration: const BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.all(Radius.circular(16))
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-             BotomNavWidget(
-              onTap: () {
-                mainScreenNotifier.pageIndex = 0;
-              },icon: Ionicons.home,
-            ),
-             BotomNavWidget(
-              onTap: () {
-                 mainScreenNotifier.pageIndex = 1;
-              },icon: Ionicons.search,
-            ),
-             BotomNavWidget(
-              onTap: () {
-                 mainScreenNotifier.pageIndex = 2;
-              },icon: Ionicons.add,
-            ),
-             BotomNavWidget(
-              onTap: () {
-                 mainScreenNotifier.pageIndex = 3;
-              },icon: Ionicons.cart,
-            ),
-             BotomNavWidget(
-              onTap: () {
-                 mainScreenNotifier.pageIndex = 4;
-              },icon: Ionicons.person,
-            ),
-          ],
-        ),
-      ),
-      )
-      )
+      bottomNavigationBar: const BottomNav()
       );
     }
   );   
   }
 }
+
